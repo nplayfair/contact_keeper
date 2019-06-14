8d60eaeb352617385111971c784428efaf731635
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-14 16:37:06 +0100

    use body parser middleware

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the contact keeper API' });
 });
