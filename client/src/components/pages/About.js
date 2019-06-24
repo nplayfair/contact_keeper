ee13509c465db605dd559651a63bcd708d596c4b
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-24 14:26:52 +0100

    Navbar and create page components

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p className="my-1">This is a full stack React app for storing contact information.</p>
+      <p className="bg-dark p">Version 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
