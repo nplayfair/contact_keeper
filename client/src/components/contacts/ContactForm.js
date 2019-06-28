b89a1252f65d711623193dbff49d4a2fb6ce797a
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-28 15:41:05 +0100

    Use form to add to temporary contacts array

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+
   const [contact, setContact] = useState({
     name: '',
     email: '',
@@ -12,20 +15,33 @@ const ContactForm = () => {
 
   const onChange = e => setContact({ ...contact, [e.target.name]: e.target.value });
 
+  const onSubmit = e => {
+    console.log('form submitted');
+    e.preventDefault();
+    contactContext.addContact(contact);
+    // Clear form
+    setContact({
+      name: '',
+      email: '',
+      phone: '',
+      type: 'personal'
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2 className="text-primary">Add Contact</h2>
       <input type="text" placeholder="Name" name="name" value={name} 
         onChange={onChange} />
       <input type="email" placeholder="Email" name="email" value={email} 
-        onChnage={onChange} />
+        onChange={onChange} />
       <input type="text" placeholder="Phone" name="phone" value={phone} 
         onChange={onChange} />
       <h5>Contact Type</h5>
       <input type="radio" name="type" value="personal" checked={type === 'personal'} 
-      /> Personal{' '}
+        onChange={onChange} /> Personal{' '}
       <input type="radio" name="type" value="professional" checked={type === 'professional'} 
-      /> Professional{' '}
+        onChange={onChange} /> Professional{' '}
       <div>
         <input type="submit" value="Add contact" className="btn btn-primary btn-block" />
       </div>
