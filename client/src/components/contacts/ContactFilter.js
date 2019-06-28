5fa5167007a14411f9f5558460208bbaa891eaa8
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-28 18:17:48 +0100

    Implement contact filtering

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect, useRef } from 'react'
+import ContactContext from '../../context/contact/contactContext'
+
+const ContactFilter = () => {
+  const contactContext = useContext(ContactContext);
+  const text = useRef('');
+
+  const { filterContacts, clearFilter, filtered } = contactContext;
+
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = '';
+    }
+  });
+
+  const onChange = e => {
+    if (text.current.value !== '') {
+      // Filter contacts
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  }
+
+  return (
+    <form>
+      <input ref={text} type="text" placeholder="Filter contacts..." 
+        onChange={onChange} />
+    </form>
+  )
+}
+
+export default ContactFilter
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,9 @@ const ContactState = props => {
         type: 'professional'
       }
     ],
-    current: null
+    current: null,
+    // Array of contacts that match the search string
+    filtered: null
   }
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -66,19 +68,27 @@ const ContactState = props => {
     dispatch({ type: UPDATE_CONTACT, payload: contact });
   }
   // Filter contacts
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  }
 
   // Clear filter
-
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  }
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}>
       { props.children }
     </ContactContext.Provider>
